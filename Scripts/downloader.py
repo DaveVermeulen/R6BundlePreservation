4c28fa0d600a33c0edc69a223536e14af657b95e
tryrodave <73838305+DaveVermeulen@users.noreply.github.com>
2026-03-11 18:40:38 +0200

    Updated downloader.py
    Refactored asset handling into method
    Added Accessory and Head/Face downloading

--- Scripts/downloader.py
+++ Scripts/downloader.py
@@ -3,6 +3,7 @@ import json
 import os
 import mesh_to_obj
 import time
+from typing import Optional
 
 asset_delivery_url = "https://assetdelivery.roblox.com/v1/asset/?id="
 json_path = "./BundleListCanceled.json"
@@ -12,53 +13,112 @@ current_directory = os.getcwd()
 final_directory = os.path.join(current_directory, r'new_folder')
 if not os.path.exists(final_directory):
    os.makedirs(final_directory)
-
+   
+def handle_asset(tex_dir, tex_id, mesh_dir: Optional[str] = None, mesh_id: Optional[int] = None):
+    # Avoid rate limits in loops
+    time.sleep(1)
+    
+    if not tex_dir or not tex_id:
+        return print("Texture path or id missing!")
+    
+    # Get texture from asset delivery and save to disk
+    texture_file_location = os.path.join(tex_dir + "\\" + str(tex_id) + ".png")
+    if not os.path.isfile(texture_file_location): # if file already exists, don't overwrite
+        texture_binary = requests.get(asset_delivery_url + str(tex_id)).content
+        with open(texture_file_location, 'wb') as texture_data:
+            texture_data.write(texture_binary)
+    
+    if not mesh_dir or not mesh_id:
+        return print("Mesh path or id missing, skipping.")
+    
+    # Get mesh from asset delivery and save to disk
+    mesh_file_location = os.path.join(mesh_dir + "\\" + str(mesh_id) + ".mesh")
+    if not os.path.isfile(mesh_file_location): # if file already exists, don't overwrite
+        mesh_binary = requests.get(asset_delivery_url + str(mesh_id)).content
+        with open(mesh_file_location, 'wb') as mesh_data:
+            mesh_data.write(mesh_binary)
+    
+        # Convert .mesh to .obj        
+        with open(mesh_file_location, 'rb') as f:
+            read_mesh_data = f.read()
+        mesh = mesh_to_obj.RobloxMeshParser.parse(read_mesh_data)
+        mesh_to_obj.mesh_to_obj(mesh, (mesh_dir + "/" + str(mesh_id) + ".obj" ), texture_file_location)
+        
+def asset_id_to_id(asset_id: str):
+    return int(asset_id.split("//").pop()) 
+        
 def handle_bundle_data(bundle_data):
-    print("handle")
+    base_dir = os.getcwd()
+    canceled_bundle = False
+        
+    # find the right dir based on bundle name or id
+    # Generally means if it has an ID it's an existing bundle, and if not it's a canceled bundle. 
+    # The difference is important as RBXM files are impossible to download straight from the site.
+    if "Id" in bundle_data:
+        print("Marketplace Bundle")
+        base_dir = final_directory
+    else:
+        print("Canceled Bundle")
+        base_dir = final_directory
+    
+    texture_dir = os.path.join(base_dir, r'Textures')
+    if not os.path.exists(texture_dir):
+        os.makedirs(texture_dir)
     
     # Handle body parts
     for body_part in bundle_data["CharacterMeshes"]:
-        # Init directories
-        texture_dir = os.path.join(final_directory, r'Textures')
-        if not os.path.exists(texture_dir):
-            os.makedirs(texture_dir)
-            
-        body_part_dir = os.path.join(final_directory, body_part["BodyPart"])
+        body_part_dir = os.path.join(base_dir, r'BodyParts', body_part["BodyPart"])
         if not os.path.exists(body_part_dir):
             os.makedirs(body_part_dir)
         
-        # Get textures and meshes from url and save them to disk
-        texture_file_location = os.path.join(texture_dir + "\\" + str(body_part['BaseTextureId']) + ".png")
-        if not os.path.isfile(texture_file_location):
-            texture_binary = requests.get(asset_delivery_url + str(body_part['BaseTextureId'])).content
-            with open(texture_file_location, 'wb') as texture_data:
-                texture_data.write(texture_binary)
-                
-        mesh_file_location = os.path.join(body_part_dir + "\\" + str(body_part['MeshId']) + ".mesh")
-        if not os.path.isfile(mesh_file_location):
-            mesh_binary = requests.get(asset_delivery_url + str(body_part['MeshId'])).content
-            with open(mesh_file_location, 'wb') as mesh_data:
-                mesh_data.write(mesh_binary)
+        handle_asset(
+            texture_dir, 
+            body_part['BaseTextureId'], 
+            body_part_dir, 
+            body_part['MeshId']
+            )
         
-            # Convert .mesh to .obj        
-            with open(mesh_file_location, 'rb') as f:
-                read_mesh_data = f.read()
-                
-            mesh = mesh_to_obj.RobloxMeshParser.parse(read_mesh_data)
-            mesh_to_obj.mesh_to_obj(mesh, (body_part_dir + "/" + str(body_part['MeshId']) + ".obj" ), texture_file_location)
+    # Handle accessories
+    for accessory in bundle_data["Accessories"]:
+        accessory_dir = os.path.join(base_dir, r'Accessories', accessory["Name"])
+        if not os.path.exists(accessory_dir):
+            os.makedirs(accessory_dir)
         
-        # Wait to avoid rate limits
-        time.sleep(1)
-        print(body_part)
+        handle_asset(
+            texture_dir, 
+            asset_id_to_id(accessory['TextureId']), 
+            accessory_dir, 
+            asset_id_to_id(accessory['MeshId'])
+            )    
+    
+    # Handle head meshes and face textures (if present) 'Head'
+    if ("HeadData" in bundle_data) and (len(bundle_data["HeadData"]) > 0):
+        for head_data in bundle_data["HeadData"]:
+            if head_data["BodyPart"] == "Head":
+                head_dir = os.path.join(base_dir, r'BodyParts', r'Head')
+                if not os.path.exists(head_dir):
+                    os.makedirs(head_dir)
+                    
+                handle_asset(
+                    texture_dir, 
+                    asset_id_to_id(head_data["TextureId"]),
+                    head_dir,
+                    asset_id_to_id(head_data["MeshId"])
+                    )
+                
+            elif head_data["BodyPart"] == "Face":
+                handle_asset(
+                    texture_dir, 
+                    asset_id_to_id(head_data["TextureId"])
+                    )
     
     #TODO:
-    # Handle accessories
-    # Handle head meshes and face textures (if present)
+    # Download RBXMs from Items when its a regular bundle 'GeneratedRXBMs'
 
 with open(json_path, 'r') as file:
     data = json.load(file)
     
-handle_bundle_data(data[6])
+handle_bundle_data(data[73])
 
 # for bundle in data:
 #     handle_bundle_data(bundle)
